0e120eb4f6fa0e06c7d69f49c5bb5862b5b7ef9c
Victor Mendes <victorutdsim7@gmail.com>
2023-04-27 15:04:57 -0300

    feat: [req - 1] implementa função read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,24 +1,13 @@
 from functools import lru_cache
 from typing import List, Dict
-
-# tuts tuts
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, "r") as file:
+        list_of_dict = [row for row in csv.DictReader(file)]
+        return list_of_dict
 
 
 def get_unique_job_types(path: str) -> List[str]:
